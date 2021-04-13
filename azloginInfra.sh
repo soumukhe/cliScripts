b5a81634bbdfff66068b65d2674c885880aeb336
soumukhe <soumukhe@cisco.com>
2021-04-13 15:17:48 -0500

    Update azloginInfra.sh

--- azloginInfra.sh
+++ azloginInfra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./source sourceMeAzInfraVars.sh 
+source ./sourceMeAzInfraVars.sh 
 
 az login --service-principal --username $USERNAME --tenant $TENANT --password $PASSWORD
 az account set -s $SUBSID
